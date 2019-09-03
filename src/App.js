c614ba19ca7b6d3f3348c2adc6977b928308fded
Jacob Moss <jacobmoss09@gmail.com>
2019-09-02 21:50:00 -0500

    working on scoring. only the first click works

--- src/App.js
+++ src/App.js
@@ -6,18 +6,43 @@ import Cards from './components/cards';
 import JumboTitle from './components/jumbotitle';
 import Space from './space.json'
 
+
+
 class App extends React.Component {
+  // state = {
+  //   Space: Space
+  // }
+  
   gamePlay(identifier) {
     //PLAYER ACCUMULATES POINTS UNTIL THEY CLICK THE SAME CARD TWICE. THIS SETS THEIR HIGH SCORE IF THIS SCORE IS > CURRENT HIGH SCORE. CARDS REARRANGE AFTER BEING CLICKED EACH TIME. MESSAGES FOR CORRECT AND INCORRECT GUESSES. SCORE AND TOP SCORE DISPLAYED (SCORE SET TO 0 IF SAME IMAGE CLICKED TWICE)
     // console.log(identifier);
-    const positions=[];
-    while (positions.length < Space.length) {
-      let randomNumber = Math.floor((Math.random()*Space.length) + 1);
-      if (positions.indexOf(randomNumber) === -1) {
-        positions.push(randomNumber);
+    let score = 0;
+    function playRound(identifier) {
+      if(!this.guessed) {
+        score++;
+        this.guessed = true;
       }
+      console.log(score);
     }
-    console.log(positions);
+
+    playRound(identifier);
+    
+    // const positions=[];
+    // while (positions.length < Space.length) {
+    //   let randomNumber = Math.floor((Math.random()*Space.length) + 1);
+    //   if (positions.indexOf(randomNumber) === -1) {
+    //     positions.push(randomNumber);
+    //   }
+    // }
+
+    // const newSpace = positions.map((position) =>
+    //   <Cards key={position}></Cards>
+      
+    // );
+    // return(
+    //   this.setState({space: newSpace}) 
+    // )
+     
     
   }
 
@@ -26,11 +51,12 @@ class App extends React.Component {
       <Wrapper name='angela'>
       <JumboTitle 
       descrip='Welcome to Space Exploration! Make sure to visit each destination ONLY ONE TIME or you will run out of fuel and perish.'>Space Game</JumboTitle>
-      {Space.map(({id, name, image}) =>
+      {Space.map(({id, name, image, guessed}) =>
         <Cards
         key={id}
         name={name}
         image = {image}
+        guessed = {guessed}
         gamePlay = {() => this.gamePlay(id)}>
         </Cards>
       )}
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -4,7 +4,7 @@ import './style.css'
 class Cards extends React.Component {
     render() {
         return (
-            <div className="card" onClick={() => this.props.gamePlay(this.props.id)}>
+            <div className="card" data-guessed={this.props.guessed} onClick={() => this.props.gamePlay(this.props.id)}>
                 <img className="card-img-top" src={this.props.image} alt={this.props.name}></img>
                 <div className="card-body">
                     <h5 className="card-title">{this.props.name}</h5>
