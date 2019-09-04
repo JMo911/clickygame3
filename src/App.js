75000cf88c0c6ab21bc9339ad0acdcf8a0aac9e0
Jacob Moss <jacobmoss09@gmail.com>
2019-09-03 21:59:31 -0500

    topScore is incrementing as expected

--- src/App.js
+++ src/App.js
@@ -6,26 +6,62 @@ import Cards from './components/cards';
 import JumboTitle from './components/jumbotitle';
 import Space from './space.json'
 
-
+let topScore = 0;
 
 class App extends React.Component {
-  // state = {
-  //   Space: Space
-  // }
+  state = {
+    Space: Space
+  }
   
-  gamePlay(identifier) {
+  handleClick = (e, id) => {
+    // access to e.target here
+    // e.preventDefault();
+    // console.log(id);
+    // console.log(Space.id);
+    Space.forEach(element => {
+      // console.log(element.id);
+      // console.log(element.guessed)
+      if (element.id === id) {
+        if (element.guessed === false) {
+          element.guessed = true;
+          topScore++;
+        }
+        
+        console.log(topScore);
+      }
+
+      
+    });
+    // if(Space.id === id) {
+    //   Space[id].setAttribute("guessed", true);
+    // }
+    // console.log(e);
+}
+
+  // gamePlay(identifier, e) {
     //PLAYER ACCUMULATES POINTS UNTIL THEY CLICK THE SAME CARD TWICE. THIS SETS THEIR HIGH SCORE IF THIS SCORE IS > CURRENT HIGH SCORE. CARDS REARRANGE AFTER BEING CLICKED EACH TIME. MESSAGES FOR CORRECT AND INCORRECT GUESSES. SCORE AND TOP SCORE DISPLAYED (SCORE SET TO 0 IF SAME IMAGE CLICKED TWICE)
     // console.log(identifier);
-    let score = 0;
-    function playRound(identifier) {
-      if(!this.guessed) {
-        score++;
-        this.guessed = true;
-      }
-      console.log(score);
-    }
+    // topScore++;
+    // console.log(e.currentTarget.id)
+    // console.log(topScore);
+    // console.log(this.state.Space.filter(space => identifier === space.id));
+    // console.log(currentTarget);
+    // if (identifier === this.state.Space.id) {
 
-    playRound(identifier);
+    // }
+    // console.log(Space.id)
+
+
+    // let score = 0;
+    // function playRound(identifier) {
+    //   if(!this.guessed) {
+    //     score++;
+    //     this.guessed = true;
+    //   }
+    //   console.log(score);
+    // }
+
+    // playRound(identifier);
     
     // const positions=[];
     // while (positions.length < Space.length) {
@@ -44,7 +80,7 @@ class App extends React.Component {
     // )
      
     
-  }
+  // }
 
   render() {
     return(
@@ -57,7 +93,7 @@ class App extends React.Component {
         name={name}
         image = {image}
         guessed = {guessed}
-        gamePlay = {() => this.gamePlay(id)}>
+        gamePlay = {((e) => this.handleClick(e, id))}>
         </Cards>
       )}
     </Wrapper>
