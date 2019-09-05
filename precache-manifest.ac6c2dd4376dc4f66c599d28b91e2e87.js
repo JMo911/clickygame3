2621c9b1a8a614308cb72aff66ce849d5a368f62
Jacob Moss <jacobmoss09@gmail.com>
2019-09-04 22:16:40 -0500

    Updates

---
+++ precache-manifest.ac6c2dd4376dc4f66c599d28b91e2e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6263c2d9b1cd0b527832aba62a403e62",
+    "url": "/clickygame3/index.html"
+  },
+  {
+    "revision": "5ce62a1645d3b59b69ae",
+    "url": "/clickygame3/static/css/main.7c1dbddc.chunk.css"
+  },
+  {
+    "revision": "00a9b86134ba02ee5d48",
+    "url": "/clickygame3/static/js/2.ca059538.chunk.js"
+  },
+  {
+    "revision": "5ce62a1645d3b59b69ae",
+    "url": "/clickygame3/static/js/main.029d78c5.chunk.js"
+  },
+  {
+    "revision": "4c6356df06ef64b6eef9",
+    "url": "/clickygame3/static/js/runtime~main.de6c1c7f.js"
+  }
+]);
